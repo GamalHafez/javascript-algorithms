fdf5c90595c77675cc999a2d160c19073e836eea
GamalHafez <gamal.k.hafez@gmail.com>
2025-12-25 10:27:11 +0200

    add fibonacci algorithm with unit tests

---
+++ fibonacci/fibonacci.js
@@ -0,0 +1,13 @@
+const fibonacci = (n) => {
+  const fib = [0, 1];
+
+  for (let i = fib.length; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+
+  return fib;
+};
+// Time Complexity: O(n)
+
+console.log(fibonacci(3)); // [0, 1, 1]
+console.log(fibonacci(4)); // [0, 1, 1, 2]
---
+++ fibonacci/fibonacci.test.js
@@ -0,0 +1,10 @@
+import fibonacci from "./fibonacci";
+
+describe("fibonacci", () => {
+  test("returns the first n Fibonacci numbers", () => {
+    expect(fibonacci(1)).toEqual([0]);
+    expect(fibonacci(2)).toEqual([0, 1]);
+    expect(fibonacci(4)).toEqual([0, 1, 1, 2]);
+    expect(fibonacci(7)).toEqual([0, 1, 1, 2, 3, 5, 8]);
+  });
+});
