48c213064585bf7f281c881570f5e6f07c86233f
GamalHafez <gamal.k.hafez@gmail.com>
2026-05-07 10:57:35 +0300

    feat(binary-search-tree): implement recursive node insertion

--- data-structures/binary-search-tree/BinarySearchTree.js
+++ data-structures/binary-search-tree/BinarySearchTree.js
@@ -1,3 +1,5 @@
+import BSTNode from "./BSTNode.js";
+
 class BinarySearchTree {
   constructor() {
     this.root = null;
@@ -5,6 +7,33 @@ class BinarySearchTree {
 
   isEmpty() {
     return this.root === null;
+  }
+
+  insert(value) {
+    const node = new BSTNode(value);
+
+    if (this.isEmpty()) {
+      this.root = node;
+      return;
+    }
+
+    this.insertNode(this.root, node);
+  }
+
+  insertNode(root, newNode) {
+    if (newNode.value < root.value) {
+      if (!root.left) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else {
+      if (!root.right) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
   }
 }
 
