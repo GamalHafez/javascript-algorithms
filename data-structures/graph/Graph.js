6cfcc4088960080674a43fc8ef645d6f9d214fc2
GamalHafez <gamal.k.hafez@gmail.com>
2026-05-09 19:34:03 +0300

    feat(graph): implement edge and vertex removal methods

--- data-structures/graph/Graph.js
+++ data-structures/graph/Graph.js
@@ -22,6 +22,23 @@ class Graph {
     return this.AdjacencyList[v1].has(v2) && this.AdjacencyList[v2].has(v1);
   }
 
+  removeEdge(v1, v2) {
+    if (!this.AdjacencyList[v1] || !this.AdjacencyList[v2]) return;
+
+    this.AdjacencyList[v1].delete(v2);
+    this.AdjacencyList[v2].delete(v1);
+  }
+
+  removeVertex(vertex) {
+    if (!this.AdjacencyList[vertex]) return;
+
+    for (let v of [...this.AdjacencyList[vertex]]) {
+      this.removeEdge(vertex, v);
+    }
+
+    delete this.AdjacencyList[vertex];
+  }
+
   display() {
     for (let vertex in this.AdjacencyList) {
       console.log(`${vertex} --> ${[...this.AdjacencyList[vertex]]}`);
