3d139eebfa26ac0f76eb9bcb0964e0e5226be081
GamalHafez <gamal.k.hafez@gmail.com>
2026-05-09 16:20:29 +0300

    feat(graph): implement undirected graph edge connections

--- data-structures/graph/Graph.js
+++ data-structures/graph/Graph.js
@@ -2,4 +2,17 @@ class Graph {
   constructor() {
     this.AdjacencyList = {};
   }
+
+  addVertex(vertex) {
+    if (this.AdjacencyList[vertex]) return;
+    this.AdjacencyList[vertex] = new Set();
+  }
+
+  addEdge(v1, v2) {
+    if (!this.AdjacencyList[v1]) this.addVertex(v1);
+    if (!this.AdjacencyList[v2]) this.addVertex(v2);
+
+    this.AdjacencyList[v1].add(v2);
+    this.AdjacencyList[v2].add(v1);
+  }
 }
